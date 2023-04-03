a7b1a41741a26f58b5c772a7c8cf780c1a87a58b
cevik <suleymancevik64@gmail.com>
2023-04-03 19:16:55 +0300

    selenide

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
         features = "./src/test/resources/features",
         glue = {"stepDefinitions"},
         dryRun = false,
-        tags = "@navigation"
+        tags = "@google"
 
 )
 public class Runner {
--- src/test/resources/features/01Selenide.feature
+++ src/test/resources/features/01Selenide.feature
@@ -1,6 +1,5 @@
-@navigation
+@google
 Feature: Selenide Demo
 
   Scenario: navigation's test
     Given navigate to "https://www.google.com"
-
